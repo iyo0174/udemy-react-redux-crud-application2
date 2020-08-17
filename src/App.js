8ec331f7b3a17698444b90c325b174658d5232d5
iyo <iyo05b6q27h@iyobank0174.onmicrosoft.com>
2020-08-17 01:42:19 +0000

    components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 
 
-function App() {
-return (
-<React.Fragment>
-<label htmlForm = "bar">bar</label>
-<input type = "text" onChange = {() => {console.log("clicked")}}/>
-</React.Fragment>
-)
+// function App() {
+// return (
+// <React.Fragment>
+// <label htmlForm = "bar">bar</label>
+// <input type = "text" onChange = {() => {console.log("clicked")}}/>
+// </React.Fragment>
+// )
+// }
+
+const App = () => {
+    return (
+        <div>
+        <Cat />
+        </div>
+        )
 }
 
+const Cat = () => {
+    return <div>mero</div>
+}
 export default App;
